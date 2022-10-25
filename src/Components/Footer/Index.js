448fa4c6edd7cd851e240362b958157f849fb761
sebwierz <sebastianwierzba@web.de>
2022-10-25 17:51:53 +0200

    footer update

--- src/Components/Footer/Index.js
+++ src/Components/Footer/Index.js
@@ -1,11 +1,19 @@
-import React from "react"
+import React from "react";
+import FooterList from "./FooterList";
+
 
 export default function Footer(){
     return(
-        <footer className="flex justify-center p-5 text-white">
-            <a href="/">Kontakt</a>
-            <a href="/">Impressum</a>
-            <a href="/">Datenschutz</a>
+        <footer className="w-full bg-orange-400 flex text-black justify-center p-2.5">
+            <div className="w-4/5 block">
+                <div className="uppercase text-l flex font-bold m-2.5 justify-center">
+                <a href="#">Kontakt</a>
+                </div>
+                <FooterList/>
+                <div id="copyright" className="flex text-gray-600 text-sm mt-2.5 mb-2.5 justify-center">
+                    <p>&copy; HackerNews 2022</p>
+                </div>
+            </div>
         </footer>
     )
 }
