714b11436ed0f4ddb9bca7c56d5c23d7d0dcde91
Clickadelic <toby.hopp@gmail.com>
2022-10-28 15:33:11 +0200

    Logo / Pagination center

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -10,10 +10,10 @@ export default function Pagination({totalPosts, postsPerPage,setCurrentPage}){
 
     
     return(
-        <div>
+        <div className="flex justify-center">
         {
             pages.map((page,index) =>{
-                return <button className="bg-lightOrange hover:bg-darkOrange text-white font-bold py-2 px-4 rounded-l m-1 m-l-10 justify-content-center " onClick={()=>setCurrentPage(page)} key={index}>{page}</button>
+                return <button className="bg-lightOrange hover:bg-darkOrange text-white font-bold py-2 px-4 rounded-md mx-1" onClick={()=>setCurrentPage(page)} key={index}>{page}</button>
             })
         }
         </div>
